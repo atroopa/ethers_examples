5599547487cc3474d20608d6042508e615148e6e
atroopa <atroopa@gmail.com>
2023-05-17 18:38:31 +0330

    file number 4 start

---
+++ examples/4_write_contract.js
@@ -0,0 +1,9 @@
+const { ethers } = require('ethers');
+
+const INFURA_ID  = 'f67125134e064cf094e2495c49323c68';
+const provider   = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_ID}`);
+
+const account1 = '';
+const account2 = '';
+
+const privateKey1 = '';
