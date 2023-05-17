7edfab7e89fc7e7acb0dd800c1493b8810cd8ab3
atroopa <atroopa@gmail.com>
2023-05-17 12:01:03 +0330

    bug fixed when change version to 5.4.0

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,13 +1,14 @@
-const {ethers} = require('ethers');
+const { ethers } = require('ethers');
 
-const INFURA_ID = '';
-const provider  = new ethers.provider.jsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+const INFURA_ID = 'f67125134e064cf094e2495c49323c68';
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
 
-const address = '';
+
+const address = '0xcA8Fa8f0b631EcdB18Cda619C4Fc9d197c8aFfCa';
 
 const main = async () => {
     const balance = await provider.getBalance(address);
-    console.log(`\n ETH Blanac of ${address} --> ${ethers.untils.formatEther(balance)} ETH`);
+    console.log(`\n ETH Blanac of ${address} --> ${ethers.utils.formatEther(balance)} ETH`);
 }
 
 main()
