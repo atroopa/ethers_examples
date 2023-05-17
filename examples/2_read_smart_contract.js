321f12a96d38602b34876769936ada37fb26f3f4
atroopa <atroopa@gmail.com>
2023-05-17 13:03:15 +0330

    balanceOf function work true in test

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -1,22 +1,30 @@
-console.clear()
+console.clear();
 const { ethers } = require('ethers');
 
-const INFURA_ID = 'f67125134e064cf094e2495c49323c68';
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+const INFURA_ID  = 'f67125134e064cf094e2495c49323c68';
+const provider   = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
 
-const ERC20_ABI = [
+const ERC20_ABI  = [
     "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
-    "function balancrOf() view returns (uint)"
+    "function balanceOf(address) view returns (uint)"
 ]
 
 const dai_contract_address = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-
-const contract = new ethers.Contract(dai_contract_address, ERC20_ABI, provider);
+const contract             = new ethers.Contract(dai_contract_address, ERC20_ABI, provider);
 
 const main = async () => {
     const name = await contract.name();
-    console.log(`name : ${name}`);
+        const symbol = await contract.symbol();
+            const totalSupply = await contract.totalSupply();
+                const balance = await contract.balanceOf('0xaAd7efd771864A5EA5c352Aa0ed0C70E716a0697');
+
+
+    console.log(`\n read from this address ${dai_contract_address} \n`);        
+        console.log(`name : ${name} \n`);
+            console.log(`symbol: ${symbol} \n`);
+                console.log(`totalSupply: ${totalSupply} \n`);
+                    console.log(`balancOf: ${balance} \n`);
 }
- main()
+ main();
